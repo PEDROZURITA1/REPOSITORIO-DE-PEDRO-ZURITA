3bcfb5339e61008782c6439e6802c17883f5af2a
PEDROZURITA1 <159149594+PEDROZURITA1@users.noreply.github.com>
2024-02-27 10:39:24 -0500

    Registro de Temperaturas Diarias
    PEDRO ZURITA

---
+++ Registro de Temperaturas Diarias.py
@@ -0,0 +1,671 @@
+# REGISTRO DE LA TEMPERATURA
+
+
+matriz_temperaturas = [
+    # Cascales
+    [
+        # Semana 1
+        {
+            "dia": "lunes",
+            "temp": 12
+        },
+        {
+            "dia": "martes",
+            "temp": 15
+        },
+        {
+            "dia": "miercoles",
+            "temp": 21
+        },
+        {
+            "dia": "jueves",
+            "temp": 42
+        },
+        {
+            "dia": "viernes",
+            "temp": 16
+        },
+        {
+            "dia": "sabado",
+            "temp": 19
+        },
+        {
+            "dia": "domingo",
+            "temp": 20
+        }
+    ],
+    [
+        # Semana 2
+        {
+            "dia": "lunes",
+            "temp": 52
+        },
+        {
+            "dia": "martes",
+            "temp": 19
+        },
+        {
+            "dia": "miercoles",
+            "temp": 51
+        },
+        {
+            "dia": "jueves",
+            "temp": 22
+        },
+        {
+            "dia": "viernes",
+            "temp": 16
+        },
+        {
+            "dia": "sabado",
+            "temp": 19
+        },
+        {
+            "dia": "domingo",
+            "temp": 20
+        }
+    ],
+    [
+        # Semana 3
+        {
+            "dia": "lunes",
+            "temp": 12
+        },
+        {
+            "dia": "martes",
+            "temp": 12
+        },
+        {
+            "dia": "miercoles",
+            "temp": 11
+        },
+        {
+            "dia": "jueves",
+            "temp": 46
+        },
+        {
+            "dia": "viernes",
+            "temp": 26
+        },
+        {
+            "dia": "sabado",
+            "temp": 19
+        },
+        {
+            "dia": "domingo",
+            "temp": 51
+        }
+    ],
+    [
+        # Semana 4
+        {
+            "dia": "lunes",
+            "temp": 12
+        },
+        {
+            "dia": "martes",
+            "temp": 15
+        },
+        {
+            "dia": "miercoles",
+            "temp": 21
+        },
+        {
+            "dia": "jueves",
+            "temp": 42
+        },
+        {
+            "dia": "viernes",
+            "temp": 16
+        },
+        {
+            "dia": "sabado",
+            "temp": 19
+        },
+        {
+            "dia": "domingo",
+            "temp": 20
+        }
+    ]
+],
+[  # Lago Agrio
+    [
+        # Semana 1
+        {
+            "dia": "lunes",
+            "temp": 12
+        },
+        {
+            "dia": "martes",
+            "temp": 15
+        },
+        {
+            "dia": "miercoles",
+            "temp": 21
+        },
+        {
+            "dia": "jueves",
+            "temp": 42
+        },
+        {
+            "dia": "viernes",
+            "temp": 16
+        },
+        {
+            "dia": "sabado",
+            "temp": 19
+        },
+        {
+            "dia": "domingo",
+            "temp": 20
+        }
+    ],
+    [
+        # Semana 2
+        {
+            "dia": "lunes",
+            "temp": 52
+        },
+        {
+            "dia": "martes",
+            "temp": 19
+        },
+        {
+            "dia": "miercoles",
+            "temp": 51
+        },
+        {
+            "dia": "jueves",
+            "temp": 22
+        },
+        {
+            "dia": "viernes",
+            "temp": 16
+        },
+        {
+            "dia": "sabado",
+            "temp": 19
+        },
+        {
+            "dia": "domingo",
+            "temp": 20
+        }
+    ],
+    [
+        # Semana 3
+
+        {
+            "dia": "lunes",
+            "temp": 12
+        },
+        {
+            "dia": "martes",
+            "temp": 15
+        },
+        {
+            "dia": "miercoles",
+            "temp": 21
+        },
+        {
+            "dia": "jueves",
+            "temp": 42
+        },
+        {
+            "dia": "viernes",
+            "temp": 26
+        },
+        {
+            "dia": "sabado",
+            "temp": 13
+        },
+        {
+            "dia": "domingo",
+            "temp": 10
+        }
+    ],
+    [
+        # Semana 4
+        {
+            "dia": "lunes",
+            "temp": 18
+        },
+        {
+            "dia": "martes",
+            "temp": 45
+        },
+        {
+            "dia": "miercoles",
+            "temp": 31
+        },
+        {
+            "dia": "jueves",
+            "temp": 42
+        },
+        {
+            "dia": "viernes",
+            "temp": 13
+        },
+        {
+            "dia": "sabado",
+            "temp": 19
+        },
+        {
+            "dia": "domingo",
+            "temp": 27
+        }
+    ]
+],
+[
+    # Sevilla
+    [
+        # Semana 1
+        {
+            "dia": "lunes",
+            "temp": 32
+        },
+        {
+            "dia": "martes",
+            "temp": 15
+        },
+        {
+            "dia": "miercoles",
+            "temp": 11
+        },
+        {
+            "dia": "jueves",
+            "temp": 42
+        },
+        {
+            "dia": "viernes",
+            "temp": 14
+        },
+        {
+            "dia": "sabado",
+            "temp": 19
+        },
+        {
+            "dia": "domingo",
+            "temp": 23
+        }
+    ],
+    [
+        # Semana 2
+        {
+            "dia": "lunes",
+            "temp": 22
+        },
+        {
+            "dia": "martes",
+            "temp": 14
+        },
+        {
+            "dia": "miercoles",
+            "temp": 21
+        },
+        {
+            "dia": "jueves",
+            "temp": 32
+        },
+        {
+            "dia": "viernes",
+            "temp": 14
+        },
+        {
+            "dia": "sabado",
+            "temp": 49
+        },
+        {
+            "dia": "domingo",
+            "temp": 22
+        }
+    ],
+    [
+        # Semana 3
+        {
+            "dia": "lunes",
+            "temp": 32
+        },
+        {
+            "dia": "martes",
+            "temp": 19
+        },
+        {
+            "dia": "miercoles",
+            "temp": 11
+        },
+        {
+            "dia": "jueves",
+            "temp": 40
+        },
+        {
+            "dia": "viernes",
+            "temp": 19
+        },
+        {
+            "dia": "sabado",
+            "temp": 29
+        },
+        {
+            "dia": "domingo",
+            "temp": 23
+        }
+    ],
+    [
+        # Semana 4
+        {
+            "dia": "lunes",
+            "temp": 13
+        },
+        {
+            "dia": "martes",
+            "temp": 14
+        },
+        {
+            "dia": "miercoles",
+            "temp": 51
+        },
+        {
+            "dia": "jueves",
+            "temp": 46
+        },
+        {
+            "dia": "viernes",
+            "temp": 26
+        },
+        {"dia": "sabado",
+         "temp": 39
+         },
+        {
+            "dia": "domingo",
+            "temp": 40
+        }
+    ]
+],
+[
+    # Quito
+    [
+        # Semana 1
+        {
+            "dia": "lunes",
+            "temp": 22
+        },
+        {
+            "dia": "martes",
+            "temp": 13
+        },
+        {
+            "dia": "miercoles",
+            "temp": 31
+        },
+        {
+            "dia": "jueves",
+            "temp": 42
+        },
+        {
+            "dia": "viernes",
+            "temp": 16
+        },
+        {
+            "dia": "sabado",
+            "temp": 39
+        },
+        {
+            "dia": "domingo",
+            "temp": 24
+        }
+    ],
+    [
+        # Semana 2
+        {
+            "dia": "lunes",
+            "temp": 62
+        },
+        {
+            "dia": "martes",
+            "temp": 19
+        },
+        {
+            "dia": "miercoles",
+            "temp": 11
+        },
+        {
+            "dia": "jueves",
+            "temp": 22
+        },
+        {
+            "dia": "viernes",
+            "temp": 16
+        },
+        {
+            "dia": "sabado",
+            "temp": 12
+        },
+        {
+            "dia": "domingo",
+            "temp": 60
+        }
+    ],
+    [
+        # Semana 3
+        {
+            "dia": "lunes",
+            "temp": 12
+        },
+        {
+            "dia": "martes",
+            "temp": 15
+        },
+        {
+            "dia": "miercoles",
+            "temp": 21
+        },
+        {
+            "dia": "jueves",
+            "temp": 42
+        },
+        {
+            "dia": "viernes",
+            "temp": 26
+        },
+        {
+            "dia": "sabado",
+            "temp": 19
+        },
+        {
+            "dia": "domingo",
+            "temp": 20
+        }
+    ],
+    [
+        # Semana 4
+        {
+            "dia": "lunes",
+            "temp": 12
+        },
+        {
+            "dia": "martes",
+            "temp": 15
+        },
+        {
+            "dia": "miercoles",
+            "temp": 21
+        },
+        {
+            "dia": "jueves",
+            "temp": 42
+        },
+        {
+            "dia": "viernes",
+            "temp": 16
+        },
+        {
+            "dia": "sabado",
+            "temp": 19
+        },
+        {
+            "dia": "domingo",
+            "temp": 20
+        }
+    ]
+],
+[
+    # Santo Domingo
+    [
+        # Semana 1
+        {
+            "dia": "lunes",
+            "temp": 12
+        },
+        {
+            "dia": "martes",
+            "temp": 15
+        },
+        {
+            "dia": "miercoles",
+            "temp": 24
+        },
+        {
+            "dia": "jueves",
+            "temp": 42
+        },
+        {
+            "dia": "viernes",
+            "temp": 26
+        },
+        {
+            "dia": "sabado",
+            "temp": 19
+        },
+        {
+            "dia": "domingo",
+            "temp": 24
+        }
+    ],
+    [
+        # Semana 2
+        {
+            "dia": "lunes",
+            "temp": 58
+        },
+        {
+            "dia": "martes",
+            "temp": 11
+        },
+        {
+            "dia": "miercoles",
+            "temp": 21
+        },
+        {
+            "dia": "jueves",
+            "temp": 28
+        },
+        {
+            "dia": "viernes",
+            "temp": 16
+        },
+        {
+            "dia": "sabado",
+            "temp": 29
+        },
+        {
+            "dia": "domingo",
+            "temp": 20
+        }
+    ],
+    [
+        # Semana 3
+        {
+            "dia": "lunes",
+            "temp": 15
+        },
+        {
+            "dia": "martes",
+            "temp": 25
+        },
+        {
+            "dia": "miercoles",
+            "temp": 23
+        },
+        {
+            "dia": "jueves",
+            "temp": 47
+        },
+        {
+            "dia": "viernes",
+            "temp": 36
+        },
+        {
+            "dia": "sabado",
+            "temp": 15
+        },
+        {
+            "dia": "domingo",
+            "temp": 40
+        }
+    ],
+    [
+        # Semana 4
+        {
+            "dia": "lunes",
+            "temp": 10
+        },
+        {
+            "dia": "martes",
+            "temp": 17
+        },
+        {
+            "dia": "miercoles",
+            "temp": 11
+        },
+        {
+            "dia": "jueves",
+            "temp": 48
+        },
+        {
+            "dia": "viernes",
+            "temp": 36
+        },
+        {
+            "dia": "sabado",
+            "temp": 16
+        },
+        {
+            "dia": "domingo",
+            "temp": 25
+        }
+    ]
+]
+
+
+def calcular_promedios_semanales(matriz_temperaturas):
+    promedios_semanales = []
+    for ciudad in matriz_temperaturas:
+        promedios_ciudad = []
+        for semana in ciudad:
+            suma_temperaturas_semana = sum(dia["temp"] for dia in semana)
+            promedio_semana = suma_temperaturas_semana / len(semana)
+            promedios_ciudad.append(promedio_semana)
+        promedios_semanales.append(promedios_ciudad)
+    return promedios_semanales
+
+
+def imprimir_promedios(promedios_ciudad):
+    for idx_semana, promedio_semana in enumerate(promedios_ciudad, start=1):
+        print(f"Semana {idx_semana}: {promedio_semana:.2f}")
+
+
+# Definición de la lista de ciudades
+ciudades = ["Cascales", "Lago Agrio", "Sevilla", "Quito", "Santo Domingo"]
+
+# Uso de las funciones
+while True:
+    ciudad_elegida = input("Por favor, ingresa el nombre de la ciudad o 'q' para salir: ").strip().capitalize()
+    if ciudad_elegida == 'Q':
+        break
+    elif ciudad_elegida in ciudades:
+        idx = ciudades.index(ciudad_elegida)
+        print(f"Promedios de temperatura para la ciudad {ciudad_elegida}:")
+        promedios_semanales_ciudad = calcular_promedios_semanales(matriz_temperaturas)[idx]
+        imprimir_promedios(promedios_semanales_ciudad)
+    else:
+        print("¡Ciudad no encontrada! Por favor, ingresa el nombre de la ciudad nuevamente.")
